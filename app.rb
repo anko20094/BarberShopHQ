1cac764257f9994010275a14ba022a5e692ddf04
anko20094 <31440971+anko20094@users.noreply.github.com>
2021-08-05 15:03:03 +0300

    Add Rekefile

--- app.rb
+++ app.rb
@@ -1,4 +1,3 @@
-#encoding: utf-8
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
@@ -7,7 +6,7 @@ require 'sinatra/activerecord'
 set :database, 'sqlite3:barbershop.db'
 
 class Client < ActiveRecord::Base
-	
+
 end
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
