08b660291a1fd5b968d1ec7abbb659508da7af90
anko20094 <31440971+anko20094@users.noreply.github.com>
2021-08-09 15:56:41 +0300

    Update app.rb

--- app.rb
+++ app.rb
@@ -21,13 +21,13 @@ class Contact < ActiveRecord::Base
 end
 
 # Задвання №1 29 урок тут
-def save_form_data_to_database (name, phone, date, barber, color)
-	Client.create :name => name, :phone => phone, :datestamp => date, :barber => barber, :color => color
-end
+# def save_form_data_to_database (name, phone, date, barber, color)
+# 	Client.create :name => name, :phone => phone, :datestamp => date, :barber => barber, :color => color
+# end
 # Задвання №2 29 урок тут
-def save_form_data_to_database_contacts (name, phone, question)
-	Contact.create :name => name, :phone => phone, :question => question
-end
+# def save_form_data_to_database_contacts (name, phone, question)
+# 	Contact.create :name => name, :phone => phone, :question => question
+# end
 
 
 before do
@@ -45,20 +45,20 @@ end
 
 post '/visit' do
 	c = Client.new params[:client]
-	c.save
-
-	
-	@error = hh.select {|key,_| params[key] == ""}.values.join(", ")
-	if @error != ''
-		return erb :visit
+	if c.save
+		erb "<h2>Вітаю, Ви записалися!</h2>"
+	else
+		erb "<h2>Помилка</h2>"
 	end
+	# @error = hh.select {|key,_| params[key] == ""}.values.join(", ")
+	# if @error != ''
+	# 	return erb :visit
+	# end
 	
-	@message = "Вітаю, Ви, #{params[:name]}, записалися! Дата візиту - #{params[:date]}"
-
 	# Задвання №1 29 урок тут
-	save_form_data_to_database(@username, @phone, @datetime, @barber, @color)
+	# save_form_data_to_database(@username, @phone, @datetime, @barber, @color)
 	# ---
-	erb "<h2><%=@message%></h2>"
+	
 end
 
 # Задвання №2 29 урок тут
@@ -70,7 +70,7 @@ post '/contacts' do
 	c = Contact.new params[:contact]
 	c.save
 
-	save_form_data_to_database_contacts(name, phone, question)
+	# save_form_data_to_database_contacts(name, phone, question)
 	erb :contacts
 end
 # ---
