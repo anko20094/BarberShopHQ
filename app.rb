8c952d7a74d82d4059e25a469dd8144bd348373a
anko20094 <31440971+anko20094@users.noreply.github.com>
2021-10-29 15:18:28 +0300

    nothing changed

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'sinatra/activerecord'
 
-set :database, {adapter: "sqlite3", database: "barbershop.db"}	
+set :database, {adapter: "sqlite3", database: "barbershop.db"}
 
 class Client < ActiveRecord::Base
 	validates :name, presence: true, length: {in: 3..20}
